dd32c6aa695d6ced968cd8acef283ab8f303539e
unknown <andonov.done@students.finki.ukim.mk>
2019-04-04 09:44:16 +0200

    Initial commit

--- program.cpp
+++ program.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 void printMyName () {
 	//TODO
+	printf("Done Andonov");
 }
 
 void printMyIndex () {
 	//TODO
+	printf("141182");
 }
 
 int main () {
 	//TODO
+	printMyName();
+	printMyIndex();
 }
